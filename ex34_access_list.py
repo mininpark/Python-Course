6cbb743096d89e642e6b403efd97dd670a547974
mininpark <parkmini.93@gmail.com>
2021-10-13 17:30:49 +0300

    9.Practice

--- ex34_access_list.py
+++ ex34_access_list.py
@@ -1,3 +1,10 @@
 animals = ['bear', 'tiger', 'penguin', 'zebra']
 bear = animals[0]
 
+for animal in animals:
+    print(animal)
+
+print(animals[2])
+
+# cardinal = one, two, three, four
+# ordinal number = firth, second, third, fourth
