0f0c7b855f5344fd639c8e53e9aa6317b663b1a0
mininpark <parkmini.93@gmail.com>
2021-12-09 22:07:57 +0300

    HW2 update 5

--- Homework2/LinRegression.py
+++ Homework2/LinRegression.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
-def Regression(X,y):
+def LinRegression(X,Y):
     # in numpy, np.inv is marix inverse, .T is transpose, @ is matrix multiplication
     XT = X.T
     inverse = np.linalg.inv(XT @ X) 
-    beta_hat = inverse @ XT @ y
-    e = y - (X @ beta_hat) # error term
+    beta_hat = inverse @ XT @ Y
+    e = Y - (X @ beta_hat) # error term
     sigma2 = (e.T @ e)/(X.shape[0]-X.shape[1])
     var = np.diag(sigma2 * inverse) # variance
     SE = np.sqrt(var).flatten() #standard error
@@ -17,4 +17,7 @@ def Regression(X,y):
     minus = beta_hat - SE * t 
     # confidence array
     CI = np.hstack([minus, plus]) 
-    output = pd.DataFrame(np.hstack([beta_hat, SE, CI]), columns=['Coefficients', 'Standard Error', 'CI-', 'CI+']) 
+    output = pd.DataFrame(np.hstack([beta_hat, SE, CI]))
+    print("Here are the results of your regression")
+    print(output)
+    return output
